d0eb58260d1ac3bc7426c2e8eb530f6ded419a6f
amiel peled <amielnoy@gmail.com>
2026-09-07 08:26:01 +0000

    הוספת תפריט ניווט צף צדי

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FloatingHeader from "@/components/dorit/FloatingHeader";
+import SectionNav from "@/components/dorit/SectionNav";
 import SecurityScroll from "@/components/dorit/SecurityScroll";
 import Hero from "@/components/dorit/Hero";
 import About from "@/components/dorit/About";
@@ -30,6 +31,7 @@ export default function Home() {
         <DetailedContactForm />
         <QuickContact />
       </main>
+      <SectionNav />
       <Footer />
     </div>
   );
