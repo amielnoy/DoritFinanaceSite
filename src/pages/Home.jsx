92ba55696e236d5f47da13b0c70b895930f552b7
amiel peled <amielnoy@gmail.com>
2026-09-07 07:11:15 +0000

    הוספת טופס יצירת קשר מהיר עם שליחת מייל

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import ServiceMatrix from "@/components/dorit/ServiceMatrix";
 import ProofCarousel from "@/components/dorit/ProofCarousel";
 import Testimonials from "@/components/dorit/Testimonials";
 import ConsultationBuilder from "@/components/dorit/ConsultationBuilder";
+import QuickContact from "@/components/dorit/QuickContact";
 import Footer from "@/components/dorit/Footer";
 
 export default function Home() {
@@ -23,6 +24,7 @@ export default function Home() {
         <ProofCarousel />
         <Testimonials />
         <ConsultationBuilder />
+        <QuickContact />
       </main>
       <Footer />
     </div>
